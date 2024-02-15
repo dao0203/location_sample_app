8d4b201c3c08a14e71b170e5af5eded5a34c153d
dao0203 <youzaizuoteng872@gmail.com>
2024-02-15 16:41:10 +0900

    add initializeprovider

--- lib/permission_gate.dart
+++ lib/permission_gate.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:location_sample_app/location_repository.dart';
 import 'package:location_sample_app/location_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final initializeProvider = FutureProvider<void>((ref) async {
+  final locationRepository = ref.read(locationRepositoryProvider);
+  await locationRepository.permission();
+  await Supabase.instance.client.auth.signInWithPassword(
+    password: dotenv.env['SUPABASE_PASSWORD']!,
+    email: dotenv.env['SUPABASE_EMAIL'],
+  );
+});
 
 class PermissionGate extends HookConsumerWidget {
   const PermissionGate({super.key});
@@ -10,7 +21,7 @@ class PermissionGate extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final locationRepository = ref.watch(locationRepositoryProvider);
     return FutureBuilder(
-        future: locationRepository.permission(),
+        future: ref.watch(initializeProvider.future),
         builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return const LocationScreen();
